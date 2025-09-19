0609f3ed7415a736bd47e177970b885538fe478d
mairodriguez0 <rodriguezbmaite@gmail.com>
2025-09-19 11:08:50 -0300

    .

--- TrabajoPractico_1/Problema_2/modules/juego_de_guerra.py
+++ TrabajoPractico_1/Problema_2/modules/juego_de_guerra.py
@@ -1,85 +1,195 @@
 # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 """
-Created on Sun Aug 21 11:23:53 2022
+Created on Thu Aug 11 09:32:57 2022
 
 @author: Cátedra de Algoritmos y Estructura de Datos
 """
 
+from mazo import Mazo, DequeEmptyError
+from carta import Carta
+import random
 
-from modulos.juego_de_guerra import juego_de_guerra
-import unittest
+N_TURNOS = 10000
 
 
-class TestJjuego_de_guerra(unittest.TestCase):
+class JuegoGuerra:
     
-    def test_resulta_gana_jugador1(self):
-        """
-        Compruebo el número de turnos de 3 partidas con
-        el jugador 1 como ganador
-        """
-
-        # jugador 1 gana la partida en el turno 137
-        self.juego_1 = juego_de_guerra(random_seed=314)
-        # jugador 1 gana la partida en el turno 638
-        self.juego_2 = juego_de_guerra(random_seed=59)
-        # jugador 1 gana la partida en el turno 1383
-        self.juego_3 = juego_de_guerra(random_seed=883)
-
-        self.juego_1.iniciar_juego(ver_partida=False)
-        self.juego_2.iniciar_juego(ver_partida=False)
-        self.juego_3.iniciar_juego(ver_partida=False)
-        
-        self.assertEqual(self.juego_1.turnos_jugados, 137)
-        self.assertEqual(self.juego_1.ganador, 'jugador 1')
-        
-        self.assertEqual(self.juego_2.turnos_jugados, 638)
-        self.assertEqual(self.juego_2.ganador, 'jugador 1')
-        
-        self.assertEqual(self.juego_3.turnos_jugados, 1383)
-        self.assertEqual(self.juego_3.ganador, 'jugador 1')
-        
+    valores = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'] 
+    palos = ['♠', '♥', '♦', '♣']
     
-    def test_resulta_gana_jugador2(self):
-        """
-        compruebo el número de turnos de 3 partidas con
-        el jugador 2 como ganador
-        """
-
-        # jugador 2 gana la partida en el turno 145
-        self.juego_4 = juego_de_guerra(random_seed=167)
-        # jugador 2 gana la partida en el turno 1112
-        self.juego_5 = juego_de_guerra(random_seed=190)
-        # jugador 2 gana la partida en el turno 1373 por Guerra
-        self.juego_6 = juego_de_guerra(random_seed=735)
-
-        self.juego_4.iniciar_juego(ver_partida=False)
-        self.juego_5.iniciar_juego(ver_partida=False)
-        self.juego_6.iniciar_juego(ver_partida=False)
+    def __init__(self, random_seed = 0):
         
-        self.assertEqual(self.juego_4.turnos_jugados, 145)
-        self.assertEqual(self.juego_4.ganador, 'jugador 2')
-        
-        self.assertEqual(self.juego_5.turnos_jugados, 1112)
-        self.assertEqual(self.juego_5.ganador, 'jugador 2')
-        
-        self.assertEqual(self.juego_6.turnos_jugados, 1373)
-        self.assertEqual(self.juego_6.ganador, 'jugador 2')
+        self._mazo_inicial = Mazo()
+        self.mazo_1 = Mazo()
+        self.mazo_2 = Mazo()
+        self._guerra = False
+        self._ganador = ''
+        self.empate = False
+        self._turno = 0
+        self._cartas_en_la_mesa = []
+        self._seed = random_seed
     
-    def test_resulta_empate(self):
-        """
-        compruebo el resultado de 2 partidas con empate
-        """
-
-        # juego empate
-        self.juego_7 = juego_de_guerra(random_seed=547)
-        self.juego_8 = juego_de_guerra(random_seed=296)
-
-        self.juego_7.iniciar_juego(ver_partida=False)
-        self.juego_8.iniciar_juego(ver_partida=False)
+    @property
+    def mazo_1(self):
+        return self._mazo_1
+    
+    @mazo_1.setter
+    def mazo_1(self, valor):
+        self._mazo_1 = valor
+    
+    @property
+    def mazo_2(self):
+        return self._mazo_2
+    
+    @mazo_2.setter
+    def mazo_2(self, valor):
+        self._mazo_2 = valor
         
-        self.assertTrue(self.juego_7.empate)
-        self.assertTrue(self.juego_8.empate)
+    @property
+    def empate(self):
+        return self._empate
     
+    @empate.setter
+    def empate(self, valor):
+        self._empate = valor
+        
+    @property
+    def turnos_jugados(self):
+        if self.empate:
+            return N_TURNOS
+        return self._turno + 1
+           
+    @property
+    def ganador(self):
+        return self._ganador
+        
+    def armar_mazo_inicial(self):
+        """representa el mazo inicial, este mazo se crea al inicio
+        de cada partida mediante combinación de los números y palos 
+        de la baraja para formar cartas
+        """
+        random.seed(self._seed)
+        cartas = [Carta(valor, palo) for valor in JuegoGuerra.valores
+                  for palo in JuegoGuerra.palos]
+        
+        #cartas_shuffled = random.sample(cartas, len(cartas))
+        random.shuffle(cartas)
+        cartas_shuffled = cartas
+        
+        for carta in cartas_shuffled:
+            self._mazo_inicial.poner_carta_arriba(carta)
+        
+        return self._mazo_inicial
     
-if __name__ == '__main__':
-    unittest.main()
+    def repartir_cartas(self):
+        """reparte el mazo inicial entre los dos jugadores"""
+        while len(self._mazo_inicial):
+            carta_1 = self._mazo_inicial.sacar_carta_arriba()
+            self.mazo_1.poner_carta_arriba(carta_1)
+            carta_2 = self._mazo_inicial.sacar_carta_arriba()
+            self.mazo_2.poner_carta_arriba(carta_2) 
+            
+        return self.mazo_1, self.mazo_2
+    
+    def iniciar_juego(self, ver_partida=True):
+        
+        self.armar_mazo_inicial()
+        self.repartir_cartas()
+        
+        self._cartas_en_la_mesa = []
+        self._turno = 0
+        
+        while len(self.mazo_1) and len(self.mazo_2) and self._turno != N_TURNOS:            
+            try:
+                if self._guerra:
+                    for _ in range(3):
+                        self._cartas_en_la_mesa.append(self.mazo_1.sacar_carta_arriba())
+                        self._cartas_en_la_mesa.append(self.mazo_2.sacar_carta_arriba())
+                
+                self._cartas_en_la_mesa.append(self.mazo_1.sacar_carta_arriba(mostrar=True))
+                self._cartas_en_la_mesa.append(self.mazo_2.sacar_carta_arriba(mostrar=True))
+            
+            except DequeEmptyError:
+                #un mazo se quedó sin cartas durante el turno
+                #declaro ganador al jugador que tiene cartas
+                #debido al orden, el jugador 1 se queda primero sin cartas
+                if len(self.mazo_1):
+                    self._ganador = 'jugador 1'
+                else:
+                    self._ganador = 'jugador 2'
+                self._guerra = False
+                if ver_partida:
+                    print(f'             ***** {self._ganador} gana la partida*****                           ')  
+                
+            else:
+                if ver_partida:
+                    self.mostrar_juego()
+               
+                if  self._cartas_en_la_mesa[-2] >  self._cartas_en_la_mesa[-1]:
+                    for carta in self._cartas_en_la_mesa:
+                        self.mazo_1.poner_carta_abajo(carta)
+                    self._cartas_en_la_mesa = []
+                    self._guerra = False
+                    if len(self.mazo_2):
+                        #si el mazo del oponente se queda sin cartas
+                        #el juego debe terminar sin incrementar
+                        #el número de turnos
+                        self._turno += 1
+                elif  self._cartas_en_la_mesa[-1] > self._cartas_en_la_mesa[-2]:
+                    for carta in self._cartas_en_la_mesa:
+                        self.mazo_2.poner_carta_abajo(carta)
+                    self._cartas_en_la_mesa = []
+                    self._guerra = False
+                    if len(self.mazo_1):                        
+                        #si el mazo del oponente se queda sin cartas
+                        #el juego debe terminar sin incrementar
+                        #el número de turnos
+                        self._turno += 1
+                else:
+                    self._guerra = True
+                    if ver_partida:
+                        print('                      **** Guerra!! ****                       ')
+            
+            finally:                       
+                if self._turno == N_TURNOS:
+                    self.empate = True
+                    if ver_partida:
+                        print('                       ***** Empate *****                           ')  
+                    
+        if self._turno != N_TURNOS and not self._ganador: 
+            if len(self.mazo_1):
+                self._ganador = 'jugador 1'
+            else:
+                self._ganador = 'jugador 2'              
+            if ver_partida:
+                print(f'           ***** {self._ganador} gana la partida*****                           ')  
+                      
+        
+        
+    def mostrar_juego(self):
+        # diferenciar las cartas del mazo y las 
+        # cartas en la mesa
+        print(f"Turno: {self._turno+1}")
+        print('jugador 1:')        
+        print(self.mazo_1)
+        print()
+        print('              ', end='')
+        for carta in self._cartas_en_la_mesa:
+            print(carta, end=' ')
+        print('\n')
+        print('jugador 2:')
+        print(self.mazo_2)            
+        print()
+        print('------------------------------------')
+        if self._ganador:
+             print(f'         ***** 3){self._ganador} gana la partida*****                           ')  
+
+
+if __name__ == "__main__":
+
+    n = random.randint(0, 1000)
+    juego = JuegoGuerra(random_seed=n)
+    juego.iniciar_juego()
+    
+    print(n)
